b4e98c7199dc3c493005590be8fa40633739e97c
Mahou <mahou.git@gmail.com>
2016-10-20 21:58:41 +0300

    second

--- src/main/java/ScanDir.java
+++ src/main/java/ScanDir.java
@@ -3,6 +3,6 @@ import org.apache.log4j.Logger;
 public class ScanDir {
     public static void main(String[] args) {
         Logger log = Logger.getLogger(ScanDir.class.getName());
-        log.error("d");
+        log.error("sas");
     }
 }
