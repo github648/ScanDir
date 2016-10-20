9aa0186f6832b53963303a572fc2d29da96358eb
Mahou <mahou.git@gmail.com>
2016-10-20 21:57:58 +0300

    first

---
+++ src/main/java/ScanDir.java
@@ -0,0 +1,8 @@
+import org.apache.log4j.Logger;
+
+public class ScanDir {
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger(ScanDir.class.getName());
+        log.error("d");
+    }
+}
